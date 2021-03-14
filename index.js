b024be960e4b50498ef2dfa195728040cd0414e9
Todd Walls <t_e_walls@yahoo.com>
2021-03-14 15:27:00 -0600

    created the skeleton of application. Need to add javascript to acquire team member's information and add HTML. I need to create tests for all four classes and I need create screencastify of functionallity and README.

---
+++ index.js
@@ -0,0 +1,9 @@
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+const path = require('path');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
